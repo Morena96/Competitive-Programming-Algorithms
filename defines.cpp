9f443f1de1fd0250e6262b8778aa361930481821
Morena96 <dowlet101@gmail.com>
2019-09-02 15:12:59 +0500

    defines.cpp

---
+++ defines.cpp
@@ -0,0 +1,26 @@
+# include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> PII;
+# define nl cout<<endl
+# define fr first
+# define sc second
+# define mk make_pair
+# define pb push_back
+# define all(a) (a).begin(),(a).end()
+# define N 300009
+# define INF 998244353
+# define rep(i,a,b) for(ll i=a;i<b;i++)
+# define test4(x,y,z,a) cout<<"x is "<<x<<"		y is "<<y<<"		z is "<<z<<"		a is "<<a<<endl
+# define test3(x,y,z) cout<<"x is "<<x<<"		y is "<<y<<"		z is "<<z<<endl
+# define test2(x,y) cout<<"x is "<<x<<"		y is "<<y<<endl
+# define test1(x) cout<<"x is "<<x<<endl
+void solve();
+bool test=false;int main(){int tst=1;if(test)cin>>tst;while(tst--)solve();}
+
+//assert cerr
+void solve(){
+	nl;
+	test1("salam");
+}
